715f73fd1611bf2425c5ae2032bd7cfa8c640912
Enzo Bouthier <enzomyo@gmail.com>
2025-09-18 01:11:48 +0200

    ajout voxel basic avec minage et creation de terre et syncro

--- connetServerClient.gd
+++ connetServerClient.gd
@@ -7,7 +7,8 @@ func _on_host_pressed() -> void:
 	peer.create_server(PORT)
 	multiplayer.multiplayer_peer = peer # Replace with function body.
 	multiplayer.peer_connected.connect(add_player)
-	add_player()
+	multiplayer.peer_disconnected.connect(del_player)
+	add_player() #si le server a un joueur
 	$CanvasLayer.hide()
 	
 func _on_join_pressed() -> void:
@@ -19,6 +20,14 @@ func _on_join_pressed() -> void:
 func add_player(id = 1):
 	var player = player_scene.instantiate()
 	player.name = str(id)
-	player.global_transform.origin = Vector3(0, 0, 0)  # ou un spawn point
+	# player.global_transform.origin = Vector3(0, 0, 0)  # ou un spawn point
 
 	call_deferred("add_child", player)
+
+#func exit_game(id):
+#	multiplayer.peer_disconnected.connect(del_player)
+#	del_player(id)  #si le server a un joueur
+
+@rpc("any_peer","call_local")
+func del_player(id):
+	get_node(str(id)).queue_free()
--- player.gd
+++ player.gd
@@ -1,52 +1,105 @@
 extends CharacterBody3D
 
-# How fast the player moves in meters per second.
-@export var speed = 14
-# The downward acceleration when in the air, in meters per second squared.
-@export var fall_acceleration = 75
+@export var speed: float = 14.0
+@export var fall_acceleration: float = 75.0
+@export var mouse_sensitivity: float = 0.002
+@onready var dig_marker = $Camera3D/digMarker
+@export var dig_interval: float = 0.1  # temps entre chaque dig/make en secondes
 
 var target_velocity = Vector3.ZERO
-
 var boolSpawnFirstTime = false
 
-@onready var cam = $Camera3D
+@onready var cam: Camera3D = $Camera3D
+@onready var voxel_terrain = get_node("/root/world/VoxelTerrain")
+@onready var voxel_tool = voxel_terrain.get_voxel_tool()
+
+var yaw := 0.0
+var pitch := 0.0
+var dig_timer: float = 0.0
+var make_timer: float = 0.0
 
 func _enter_tree() -> void:
 	set_multiplayer_authority(name.to_int())
 
 func _ready() -> void:
 	cam.current = is_multiplayer_authority()
+	if is_multiplayer_authority():
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if not is_multiplayer_authority():
+		return
+
+	if event is InputEventMouseMotion:
+		yaw -= event.relative.x * mouse_sensitivity
+		pitch -= event.relative.y * mouse_sensitivity
+		pitch = clamp(pitch, -1.5, 1.5)
+
+		rotation.y = yaw
+		cam.rotation.x = pitch
+
+	if event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _physics_process(delta):
-	if is_multiplayer_authority():
-		if boolSpawnFirstTime == false:
-			global_position = Vector3(5,5,5)
-			boolSpawnFirstTime = true
-		
-		var direction = Vector3.ZERO
+	if not is_multiplayer_authority():
+		return
 
-		if Input.is_action_pressed("ui_right"):
-			direction.x += 1
-		if Input.is_action_pressed("ui_left"):
-			direction.x -= 1
-		if Input.is_action_pressed("ui_down"):
-			direction.z += 1
-		if Input.is_action_pressed("ui_up"):
-			direction.z -= 1
+	if not boolSpawnFirstTime:
+		global_position = Vector3(1,50,1)
+		boolSpawnFirstTime = true
 
-		if direction != Vector3.ZERO:
-			direction = direction.normalized()
-			# Setting the basis property will affect the rotation of the node.
-			# $Pivot.basis = Basis.looking_at(direction)
+	# --- Movement ---
+	var direction = Vector3.ZERO
+	if Input.is_action_pressed("right"):
+		direction.x += 1
+	if Input.is_action_pressed("left"):
+		direction.x -= 1
+	if Input.is_action_pressed("down"):
+		direction.z += 1
+	if Input.is_action_pressed("up"):
+		direction.z -= 1
 
-		# Ground Velocity
-		target_velocity.x = direction.x * speed
-		target_velocity.z = direction.z * speed
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		direction = (global_transform.basis * direction).normalized()
 
-		# Vertical Velocity
-		if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
-			target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
 
-		# Moving the Character
-		velocity = target_velocity
-		move_and_slide()
+	if not is_on_floor():
+		target_velocity.y -= fall_acceleration * delta
+	else:
+		target_velocity.y = 0.0
+
+	velocity = target_velocity
+	move_and_slide()
+
+	
+	# Dig
+	if Input.is_action_pressed("dig"):
+		dig_timer -= delta
+		if dig_timer <= 0.0:
+			voxel_tool.mode = VoxelTool.MODE_REMOVE
+			voxel_tool.grow_sphere(dig_marker.global_position, 5.0, 2)
+			rpc("rpc_dig", dig_marker.global_position)
+			dig_timer = dig_interval
+
+	# Make
+	if Input.is_action_pressed("make"):
+		make_timer -= delta
+		if make_timer <= 0.0:
+			voxel_tool.mode = VoxelTool.MODE_ADD
+			voxel_tool.grow_sphere(dig_marker.global_position, 5.0, 2)
+			rpc("rpc_make", dig_marker.global_position)
+			make_timer = dig_interval
+
+@rpc("authority", "call_remote")
+func rpc_dig(voxel_pos: Vector3):
+	voxel_tool.mode = VoxelTool.MODE_REMOVE
+	voxel_tool.grow_sphere(voxel_pos, 5.0, 2)
+
+@rpc("authority", "call_remote")
+func rpc_make(voxel_pos: Vector3):
+	voxel_tool.mode = VoxelTool.MODE_ADD
+	voxel_tool.grow_sphere(voxel_pos, 5.0, 2)
